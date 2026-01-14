5674898110afe8eef7baf1750eb29cf0540335a5
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2026-01-14 19:13:14 +0100

    1

--- invitados.js
+++ invitados.js
@@ -1,6 +1,6 @@
 // invitados.js
 const invitados = [
-    { id: 1, nombre: "Familia García", pases: 4 },
+    { id: 1, nombre: "Armando Palma", pases: 1 },
     { id: 2, nombre: "María López", pases: 2 },
     { id: 3, nombre: "Carlos y Ana", pases: 3 },
     { id: 4, nombre: "José Martínez", pases: 1 },
